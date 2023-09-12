0445e457a3afa30e7f18f7b591de993fdfba383c
EduNgaira <ngairaedwin@gmail.com>
2023-09-12 10:54:37 -0700

    Changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,72 @@
+#!/usr/bin/env python3
 """Parent file from which all files will inherit from"""
 import uuid
+import sqlalchemy
+import models
 from models import storage
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+if models.storage_t == "db":
+	Base = declarative_base()
+else:
+	Base = object
 
 class BaseModel:
 """Parent Class"""
+	if models.storage_t == "db":
+		id = Column(String(60), primary_key=True)
+		created_at = Column(DateTime, default=datetime.utcnow)
+		updated_at = Column(DateTime, default=datetime.utcnow)
 
-	def __init__(self, *args, **kwargs**):
-		"""instantiate a unique id, and time stamps for the time a user is created and updated"""
-		self.created_at=datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-		self.updated_at=datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-		self.id=str(uuid.uuid4())
-		
-		if len(kwargs)!=0;
-			for x,y in kwargs.items():
-				if x=="created_at" or x=="updated_at":
-					self.__dict__[x]=datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-				else:
-					self.__dict__[x]=y
-		else:
-			storage.new(self)
+	def __init__(self, *args, **kwargs):
+        	"""Initialization of the base model"""
+        	if kwargs:
+            		for key, value in kwargs.items():
+                		if key != "__class__":
+                    			setattr(self, key, value)
+            		if kwargs.get("created_at", None) and type(self.created_at) is str:
+                		self.created_at = datetime.strptime(kwargs["created_at"], time)
+            		else:
+                		self.created_at = datetime.utcnow()
+            		if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                		self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            		else:
+                		self.updated_at = datetime.utcnow()
+            		if kwargs.get("id", None) is None:
+                		self.id = str(uuid.uuid4())
+	else:
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.utcnow()
+		self.updated_at = self.created_at
 
-	def save_update(self)
-		self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-		storage.save()
-	def save_to_dict(self):
-		rdict=self.__class__.copy()
-		rdict["created_at"]=self.created_at
-		rdict["updated_at"]=self.updated_at
-		rdict["__class__"]=self.__class__.__name__
 	def __str__(self):
-		return "[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__)
+        	"""String representation of the BaseModel class"""
+        	return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
+
+	def save(self):
+        	"""updates the attribute 'updated_at' with the current datetime"""
+        	self.updated_at = datetime.utcnow()
+        	models.storage.new(self)
+        	models.storage.save()
+
+	def to_dict(self, save_fs=None):
+        	"""returns a dictionary containing all keys/values of the instance"""
+        	new_dict = self.__dict__.copy()
+        	if "created_at" in new_dict:
+			new_dict["created_at"] = new_dict["created_at"].strftime(time)
+		if "updated_at" in new_dict:
+			new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+			new_dict["__class__"] = self.__class__.__name__
+		if "_sa_instance_state" in new_dict:
+			del new_dict["_sa_instance_state"]
+		if save_fs is None:
+			if "password" in new_dict:
+				del new_dict["password"]
+			return new_dict
+
+	def delete(self):
+        	"""delete the current instance from the storage"""
+        	models.storage.delete(self)

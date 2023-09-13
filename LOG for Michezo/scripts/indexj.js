2779701a045b2ec32e1ae53fc589d5c7894613a6
Brianmanoti <brianmanoti254@gmail.com>
2023-09-13 05:56:40 +0300

    MichezoOnlineNews version 1.0.0

--- LOG for Michezo/scripts/indexj.js
+++ LOG for Michezo/scripts/indexj.js
@@ -1,8 +1,20 @@
-let profileDropdownList = document.querySelector(".profile-dropdown-list");
-let btn = document.querySelector(".profile-dropdown-btn");
-
-const toggle = ()=> profileDropdownList.classList.toggle("active");
-
-window.addEventListener('click',function(e){
-    if(!btn.contains(e.target))profileDropdownList.classList.remove("active")
-});
+document.addEventListener('DOMContentLoaded', function () {
+    let profileDropdownList = document.querySelector(".profile-dropdown-list");
+    let btn = document.querySelector(".profile-dropdown-btn");
+  
+    const toggle = () => profileDropdownList.classList.toggle("active");
+  
+    // Add a click event listener to the profile-dropdown-btn
+    btn.addEventListener('click', function (e) {
+      e.stopPropagation(); // Prevent the click event from propagating to the window
+      toggle(); // Toggle the active class
+    });
+  
+    // Add a click event listener to the window to close the dropdown when clicking outside of it
+    window.addEventListener('click', function (e) {
+      if (!profileDropdownList.contains(e.target) && e.target !== btn) {
+        profileDropdownList.classList.remove("active");
+      }
+    });
+  });
+  

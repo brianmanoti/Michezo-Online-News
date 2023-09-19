7c433c21b16dd17c4063de63eee5b1cd0536a636
Brianmanoti <brianmanoti254@gmail.com>
2023-09-19 09:36:47 +0300

    Fixed the icon close efect

--- LOG for Michezo/script.js
+++ LOG for Michezo/script.js
@@ -2,7 +2,9 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+const iconClose = document.getElementById('icon-close');
+
+
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
